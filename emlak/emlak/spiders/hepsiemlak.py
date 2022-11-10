9a0ae4ace071b2fa96760503150db2f31cbef99d
Dmitrii_Kalmanovich <dklmn@mail.ru>
2022-11-11 00:04:20 +0300

    Scrapy project

--- emlak/emlak/spiders/hepsiemlak.py
+++ emlak/emlak/spiders/hepsiemlak.py
@@ -1,19 +1,32 @@
 import json
 import scrapy
+import time
 from ..items import EmlakItem
 
 
 class HepsiemlakSpider(scrapy.Spider):
     name = 'hepsiemlak'
     allowed_domains = ['hepsiemlak.com']
-    start_urls = ['https://www.hepsiemlak.com/api/realty-list?urlCriteria=antalya%2Ckepez%2Ckonyaalti%2Cmuratpasa%2Ckiralik%2Ckonut%2C1-1%2C2-1%2C3-1&fillIntentUrls=false']
+    baseURL = 'https://www.hepsiemlak.com/api/realty-list/antalya-kiralik?counties=kepez,konyaalti,muratpasa&page='
+    start_urls = [baseURL+'1']
 
     def parse(self, response):
-        fld_lst = ['id', 'price', 'createDate']
+        fld_lst = {'id', 'age', 'price', 'createDate', 'updatedDate', 'mapLocation/lon', 'mapLocation/lat',
+                   'age', 'city/id', 'city/name', 'county/id', 'county/name', 'district/id', 'district/name',
+                   'sqm/netSqm', 'roomAndLivingRoom', 'floor/count', 'detailDescription'}
         data = json.loads(response.body)
         lst = data['realtyList']
         for adv in lst:
-            item = EmlakItem()
+            d_ret = dict()
             for fld in fld_lst:
-                item[fld] = adv[fld]
-            yield item
+                flds_down = fld.split('/')
+                key = fld.replace('/', '_')
+                d_ret[key] = adv[flds_down[0]] if len(flds_down) == 1 else adv[flds_down[0]][flds_down[1]]
+            yield d_ret
+
+        totalPages, page = data['totalPages'], data['page']
+
+        if page <= 10: #page != totalPages &
+            next_page_url = self.baseURL+str((page+1))
+            time.sleep(1)
+            yield response.follow(next_page_url, callback=self.parse)
